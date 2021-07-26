9f3c41adc34b032972fc8848814777388f07a318
nickchen1998 <61369035+nickchen1998@users.noreply.github.com>
2021-07-26 10:36:35 +0800

    Add files via upload

---
+++ 單周 python 網路爬蟲挑戰賽/Day003_API 串接與資料存取 - 權限驗證.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 09:28:37 2021
+
+@author: nick
+"""
+
+import requests, json, time
+import pandas as pd
+
+key = "481b713073486a8cad4158747de7b75b"
+city = "Taipei"
+api = "https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={key}".format(city=city, key=key)
+
+data = requests.get(api)
+result = json.loads(data.text)["list"]
+
+result_lst = []
+
+for item in result:
+    result_lst.append([])
+    result_lst[len(result_lst)-1].append(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(item['dt'])))) # 加入預測時間
+    result_lst[len(result_lst)-1].append(item['main']['temp']) # 加入預測溫度
+    result_lst[len(result_lst)-1].append(item['main']['feels_like']) # 加入體感溫度
+    result_lst[len(result_lst)-1].append(item['main']['temp_min']) # 加入預測最小溫度
+    result_lst[len(result_lst)-1].append(item['main']['temp_max']) # 加入預測最大溫度
+
+columns = ["預測時間", "預測溫度", "預測體感溫度", "預測最小溫度", "預測最大溫度"]
+
+
+df = pd.DataFrame(result_lst, columns=columns)
+sort_df = df.sort_values(by="預測溫度", ascending=False)
+print(sort_df.head(1))
+
